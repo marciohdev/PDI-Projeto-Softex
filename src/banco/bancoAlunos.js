68263bcfd24a58f80ff84cea70f14eade006e746
Marcio Holanda <marcioh.dev@gmail.com>
2023-12-02 17:12:37 -0300

    Paginação funcionando, necessário reajustar métodos put e delete para reescrever e acessar arquivo json

--- src/banco/bancoAlunos.js
+++
@@ -1,3 +0,0 @@
-const bancoAlunos = []
-
-module.exports = bancoAlunos
--- src/controladores/alunoControlador.js
+++ src/controladores/alunoControlador.js
@@ -1,25 +1,27 @@
 const AlunoModel = require("../modelos/aluno")
-const bancoAlunos = require("../banco/bancoAlunos")
+const jsonAlunos = require("../banco/alunos.json")
+const fs = require("fs/promises")
+
+
 
 
 function cadastrarAluno(req, res) {
     try {
 
-        let idAluno = bancoAlunos.length + 1;
+        let idAluno = jsonAlunos.length + 1;
 
         const { nomeAluno, idade, dataNascimento, endereco, moraCom, qtdIrmaos, responsavel } = req.body;
-        console.log(bancoAlunos.length)
 
-        const alunoEncontrado = bancoAlunos.find(aluno => aluno.idAluno === Number(idAluno));
-        console.log(alunoEncontrado)
+        const alunoEncontrado = jsonAlunos.find(aluno => aluno.idAluno === Number(idAluno));
 
         if (alunoEncontrado) {
             return res.status(201).json({ mensagem: "Aluno já existe" })
         }
 
         const novoAluno = new AlunoModel(idAluno, nomeAluno, idade, dataNascimento, endereco, moraCom, qtdIrmaos, responsavel);
-        bancoAlunos.push(novoAluno)
-        console.log(novoAluno)
+        jsonAlunos.push(novoAluno)
+        fs.writeFile("./src/banco/alunos.json", JSON.stringify(jsonAlunos, null, 4))//escreve no json
+
         return res.status(201).json({ mensagem: "Aluno cadastrado com sucesso" })
 
     } catch (error) {
@@ -65,14 +67,13 @@ function deletarAluno(req, res) {
     try {
         const { id } = req.params;
 
-        const alunoEncontrado = bancoAlunos.find(aluno => aluno.idAluno === Number(id))
-        if (!alunoEncontrado) {
+        const alunoEncontrado = bancoAlunos.findIndex(aluno => aluno.idAluno === Number(id))
+
+        if (alunoEncontrado === -1) {
             return res.status(404).json({ mensagem: "Aluno não encontrado, tente um id válido" })
         }
 
-        const alunoIndex = bancoAlunos.findIndex(aluno => aluno.idAluno === Number(id))
         bancoAlunos.splice(alunoIndex, 1)
-        console.log(bancoAlunos)
         res.status(200).json({ mensagem: "Aluno excluído com sucesso!" })
     } catch (error) {
         return res.status(500).json({ mensagem: "Erro do servidor, aluno não removido." })
@@ -83,7 +84,43 @@ function deletarAluno(req, res) {
 
 function listarAluno(req, res) {
     try {
-        return res.status(200).json(bancoAlunos)
+        const urlAtual = req.path;
+        console.log(req)
+
+        if (jsonAlunos.length === 0) {
+            return res.status(404).json({ mensagem: "Não há registros em Alunos." })
+        }
+
+        //PAGINACAO - Usando Query String
+        //http://localhost:3000/aluno?limit=10&offset
+        let { limit, offset } = req.query;
+
+        limit = Number(limit) || 5;
+        offset = Number(offset) || 0;
+
+        const total = jsonAlunos.length
+
+        let paginaAtual = [];
+        paginaAtual = jsonAlunos.slice(offset, offset + limit)
+
+        //criando as urls proxima e anterior
+        const proximo = offset + limit;
+        const proximoUrl = proximo < total ? `${urlAtual}?limit=${limit}&offset=${proximo}` : null;
+
+
+        const anterior = proximo - limit <= 0 ? null : offset - limit;
+        const anteriorUrl =
+            anterior != null ? `${urlAtual}?limit=${limit}&offset=${anterior}` : null
+
+        return res.status(200).json({
+            proximoUrl,
+            anteriorUrl,
+            limit,
+            offset,
+            total,
+            paginaAtual
+        });
+
     } catch (error) {
         return res.status(500).json({ mensagem: "Erro do servidor. Não foi possível listar os alunos." })
     }
